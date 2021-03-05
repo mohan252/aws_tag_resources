62ad0b7c5d0e1ac85ed3e6b64f4a80b8f1af646e
Mohan <mohthiru@amazon.com>
2021-03-05 12:24:05 -0500

    updated other resources

--- tags.py
+++ tags.py
@@ -91,6 +91,149 @@ def get_snapshots(client):
     )
 
 
+def get_security_groups(client):
+    return list(
+        map(
+            lambda x: {
+                "resource-id": x["GroupId"],
+                "type": "security_group",
+                "tags": x.get("Tags", None),
+            },
+            client.describe_security_groups()["SecurityGroups"],
+        )
+    )
+
+
+def get_volumes(client):
+    return list(
+        map(
+            lambda x: {
+                "resource-id": x["VolumeId"],
+                "type": "volume",
+                "tags": x.get("Tags", None),
+            },
+            client.describe_volumes()["Volumes"],
+        )
+    )
+
+
+def get_customer_gateways(client):
+    return list(
+        map(
+            lambda x: {
+                "resource-id": x["CustomerGatewayId"],
+                "type": "customer_gateway",
+                "tags": x.get("Tags", None),
+            },
+            client.describe_customer_gateways()["CustomerGateways"],
+        )
+    )
+
+
+def get_dhcp_options(client):
+    return list(
+        map(
+            lambda x: {
+                "resource-id": x["DhcpOptionsId"],
+                "type": "dhcp_option",
+                "tags": x.get("Tags", None),
+            },
+            client.describe_dhcp_options()["DhcpOptions"],
+        )
+    )
+
+
+def get_internet_gateways(client):
+    return list(
+        map(
+            lambda x: {
+                "resource-id": x["InternetGatewayId"],
+                "type": "internet_gateway",
+                "tags": x.get("Tags", None),
+            },
+            client.describe_internet_gateways()["InternetGateways"],
+        )
+    )
+
+
+def get_network_acls(client):
+    return list(
+        map(
+            lambda x: {
+                "resource-id": x["NetworkAclId"],
+                "type": "network_acl",
+                "tags": x.get("Tags", None),
+            },
+            client.describe_network_acls()["NetworkAcls"],
+        )
+    )
+
+
+def get_network_interfaces(client):
+    return list(
+        map(
+            lambda x: {
+                "resource-id": x["NetworkInterfaceId"],
+                "type": "network_interface",
+                "tags": x.get("Tags", None),
+            },
+            client.describe_network_interfaces()["NetworkInterfaces"],
+        )
+    )
+
+
+def get_reserved_instances(client):
+    return list(
+        map(
+            lambda x: {
+                "resource-id": x["ReservedInstancesId"],
+                "type": "reserved_instance",
+                "tags": x.get("Tags", None),
+            },
+            client.describe_reserved_instances()["ReservedInstances"],
+        )
+    )
+
+
+def get_spot_instances_requests(client):
+    return list(
+        map(
+            lambda x: {
+                "resource-id": x["SpotInstanceRequestId"],
+                "type": "spot_instances_requests",
+                "tags": x.get("Tags", None),
+            },
+            client.describe_spot_instance_requests()["SpotInstanceRequests"],
+        )
+    )
+
+
+def get_vpn_connections(client):
+    return list(
+        map(
+            lambda x: {
+                "resource-id": x["VpnConnectionId"],
+                "type": "vpn_connection",
+                "tags": x.get("Tags", None),
+            },
+            client.describe_vpn_connections()["VpnConnections"],
+        )
+    )
+
+
+def get_vpn_gateways(client):
+    return list(
+        map(
+            lambda x: {
+                "resource-id": x["VpnGatewayId"],
+                "type": "vpn_gateway",
+                "tags": x.get("Tags", None),
+            },
+            client.describe_vpn_gateways()["VpnGateways"],
+        )
+    )
+
+
 resource_mappings = {
     "vpc": get_vpcs,
     "image": get_images,
@@ -98,6 +241,17 @@ resource_mappings = {
     "route-table": get_route_tables,
     "snapshot": get_snapshots,
     "instance": get_instances,
+    "security-group": get_security_groups,
+    "volume": get_volumes,
+    "customer-gateway": get_customer_gateways,
+    "dhcp-options": get_dhcp_options,
+    "internet-gateway": get_internet_gateways,
+    "network-acl": get_network_acls,
+    "network-interface": get_network_interfaces,
+    "reserved-instances": get_reserved_instances,
+    "spot-instances-request": get_spot_instances_requests,
+    "vpn-connection": get_vpn_connections,
+    "vpn-gateway": get_vpn_gateways,
 }
 
 
@@ -110,53 +264,10 @@ def load_resources():
     resource_types = json.load(open("./resource_types.json"))["resource_types"]
 
     resources = []
-
     for resource_type in resource_types:
         resources.append(resource_mappings[resource_type](ec2_client))
 
     resources = [item for sublist in resources for item in sublist]
-    print("resources: ", resources)
-
-    # print(">>>>>>>>>>    EC2 Instances    >>>>>>>>>>>>")
-    # instances = get_instances(ec2_client)
-    # print("instances: ", instances)
-
-    # # for reservation in ec2_client.describe_instances()["Reservations"]:
-    # #     for instance in reservation["Instances"]:
-    # #         print(
-    # #             "Id: {0}\nTags: {1}\n".format(
-    # #                 instance["InstanceId"],
-    # #                 instance.get("Tags", None),
-    # #             )
-    # #         )
-
-    # # vpcs
-    # print(">>>>>>>>>>    Vpcs    >>>>>>>>>>>>")
-    # vpcs = get_vpcs(ec2_client)
-    # print("vpcs: ", vpcs)
-
-    # # for vpc in ec2_client.describe_vpcs()["Vpcs"]:
-    # #     print(
-    # #         "Id: {0}\nTags: {1}\n".format(
-    # #             vpc["VpcId"],
-    # #             vpc["Tags"],
-    # #         )
-    # #     )
-
-    # print(">>>>>>>>>>    Images    >>>>>>>>>>>>")
-    # images = get_images(ec2_client)
-    # print("images: ", images)
-
-    # print(">>>>>>>>>>    Subnets    >>>>>>>>>>>>")
-    # images = get_subnets(ec2_client)
-    # print("images: ", images)
-
-    # print(">>>>>>>>>>    RouteTables    >>>>>>>>>>>>")
-    # route_tables = get_route_tables(ec2_client)
-    # print("route_tables: ", route_tables)
-
-    # print(">>>>>>>>>>    Snapshots    >>>>>>>>>>>>")
-    # snapshots = get_snapshots(ec2_client)
-    # print("snapshots: ", snapshots)
-
+    for resource in resources:
+        print(json.dumps(resource))
     continue_prompt()
